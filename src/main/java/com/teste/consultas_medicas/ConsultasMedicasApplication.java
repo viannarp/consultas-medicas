9aa564e947b8d6b9619d0c9ebb7ef70d7d616cd6
viannarp <viannarp@gmail.com>
2024-08-29 11:39:25 -0300

    Versão inicial do projeto

---
+++ src/main/java/com/teste/consultas_medicas/ConsultasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.teste.consultas_medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultasMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/consultas_medicas/ConsultasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.consultas_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
